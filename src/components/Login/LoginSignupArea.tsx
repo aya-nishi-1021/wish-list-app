d5e5475320c4b256f7fc96238f406b7838a8e30d
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-01-27 12:59:01 +0900

    feat: LoginSignupArea component

---
+++ src/components/Login/LoginSignupArea.tsx
@@ -0,0 +1,27 @@
+import '../../assets/styles/pages/login.scss';
+import IconGoogle from '../../assets/images/icon_google.svg';
+
+const LoginSignupArea: React.FC = () => (
+    <div className="login-signup__wrapper">
+      <h3 className="login-signup__sub-title">ログインまたは新規登録</h3>
+      <div className="login-signup__input-wrapper">
+        <input className="login-signup__input" type="text" name="email" placeholder="email" />
+        <input className="login-signup__input" type="password" name="password" placeholder="password" />
+        <div className="login-signup__reset-password-link-wrapper">
+          <a className="login-signup__reset-password-link" href="/#">
+            パスワードをお忘れですか？
+          </a>
+        </div>
+      </div>
+      <button className="login-signup__submit-button" type="submit">
+        続行する
+      </button>
+      <div className="login-signup__border">または</div>
+      <button className="login-signup__sns" type="button">
+        <img className="login-signup__sns-icon" src={IconGoogle} alt="google" />
+        <span className="login-signup__sns-text">Google で続行</span>
+      </button>
+    </div>
+  );
+
+export default LoginSignupArea;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,29 +1,10 @@
 import '../assets/styles/pages/login.scss';
-import IconGoogle from '../assets/images/icon_google.svg';
+import LoginSignupArea from '../components/Login/LoginSignupArea';
 
 const Login: React.FC = () => (
   <div className="login-signup">
     <h2 className="login-signup__title">Food Wish List</h2>
-    <div className="login-signup__wrapper">
-      <h3 className="login-signup__sub-title">ログインまたは新規登録</h3>
-      <div className="login-signup__input-wrapper">
-        <input className="login-signup__input" type="text" name="email" placeholder="email" />
-        <input className="login-signup__input" type="password" name="password" placeholder="password" />
-        <div className="login-signup__reset-password-link-wrapper">
-          <a className="login-signup__reset-password-link" href="/#">
-            パスワードをお忘れですか？
-          </a>
-        </div>
-      </div>
-      <button className="login-signup__submit-button" type="submit">
-        続行する
-      </button>
-      <div className="login-signup__border">または</div>
-      <button className="login-signup__sns" type="button">
-        <img className="login-signup__sns-icon" src={IconGoogle} alt="google" />
-        <span className="login-signup__sns-text">Google で続行</span>
-      </button>
-    </div>
+    <LoginSignupArea />
   </div>
 );
 
