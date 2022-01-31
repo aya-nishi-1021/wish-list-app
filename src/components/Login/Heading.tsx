ef8947152f705f5a3335b92607e39d9822ab6859
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-01-31 11:40:40 +0900

    feat: Heading component props isBackButtonShow

--- src/components/Login/Heading.tsx
+++ src/components/Login/Heading.tsx
@@ -3,19 +3,24 @@ import IconArrow from '@/assets/images/icon_arrow.svg';
 
 type Props = {
   text: string;
+  isBackButtonShow?: boolean;
 };
 
-const Heading: React.FC<Props> = (props) => {
-  const { text } = props;
-
-  return (
+const Heading: React.FC<Props> = ({ text, isBackButtonShow = false }) => (
     <div className="login-signup-area-heading">
-      <button className="login-signup-area-heading__back-button" type="button">
-        <img src={IconArrow} alt="back login page" />
-      </button>
-      <h3 className="login-signup-area-heading__text">{text}</h3>
+      {isBackButtonShow && (
+        <button className="login-signup-area-heading__back-button" type="button">
+          <img src={IconArrow} alt="back login page" />
+        </button>
+      )}
+      <h3
+        className={`login-signup-area-heading__text ${
+          isBackButtonShow ? 'login-signup-area-heading__text--back-button-show' : ''
+        }`}
+      >
+        {text}
+      </h3>
     </div>
   );
-};
 
 export default Heading;
