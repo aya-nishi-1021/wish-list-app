c260543004a4c0e35b1d95e560b09880e16cd242
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-01-31 11:01:22 +0900

    feat: IconArrow and markup

--- src/components/Login/Heading.tsx
+++ src/components/Login/Heading.tsx
@@ -1,4 +1,5 @@
 import '@/assets/styles/components/Login/Heading.scss';
+import IconArrow from '@/assets/images/icon_arrow.svg';
 
 type Props = {
   text: string;
@@ -7,7 +8,14 @@ type Props = {
 const Heading: React.FC<Props> = (props) => {
   const { text } = props;
 
-  return <h3 className="login-signup-area__heading">{text}</h3>;
+  return (
+    <div className="login-signup-area-heading">
+      <button className="login-signup-area-heading__back-button" type="button">
+        <img src={IconArrow} alt="back login page" />
+      </button>
+      <h3 className="login-signup-area-heading__text">{text}</h3>
+    </div>
+  );
 };
 
 export default Heading;
