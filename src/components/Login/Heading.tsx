d62c589b26031c9ed5014cdcdc65ced7d3ac6b1b
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-01-31 11:57:02 +0900

    feat: Heading component back-button click event

--- src/components/Login/Heading.tsx
+++ src/components/Login/Heading.tsx
@@ -7,20 +7,20 @@ type Props = {
 };
 
 const Heading: React.FC<Props> = ({ text, isBackButtonShow = false }) => (
-    <div className="login-signup-area-heading">
-      {isBackButtonShow && (
-        <button className="login-signup-area-heading__back-button" type="button">
-          <img src={IconArrow} alt="back login page" />
-        </button>
-      )}
-      <h3
-        className={`login-signup-area-heading__text ${
-          isBackButtonShow ? 'login-signup-area-heading__text--back-button-show' : ''
-        }`}
-      >
-        {text}
-      </h3>
-    </div>
-  );
+  <div className="login-signup-area-heading">
+    {isBackButtonShow && (
+      <button className="login-signup-area-heading__back-button" type="button" onClick={() => window.history.back()}>
+        <img src={IconArrow} alt="back login page" />
+      </button>
+    )}
+    <h3
+      className={`login-signup-area-heading__text ${
+        isBackButtonShow ? 'login-signup-area-heading__text--back-button-show' : ''
+      }`}
+    >
+      {text}
+    </h3>
+  </div>
+);
 
 export default Heading;
