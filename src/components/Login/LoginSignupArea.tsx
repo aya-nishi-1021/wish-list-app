51ba4f513a9b6ab901ea5c90c62e122cf881189a
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-02-08 13:42:03 +0900

    fix: LoginSignupAreaContent import path

--- src/components/Login/LoginSignupArea.tsx
+++ src/components/Login/LoginSignupArea.tsx
@@ -1,6 +1,6 @@
 import '@/assets/styles/components/Login/LoginSignupArea.scss';
 import LoginSignupAreaHeading from '@/components/Login/LoginSignupAreaHeading';
-import LoginSignupAreaContent from './LoginSignupAreaContent';
+import LoginSignupAreaContent from '@/components/Login/LoginSignupAreaContent';
 
 const LoginSignupArea: React.FC = () => (
   <div className="login-signup-area">
