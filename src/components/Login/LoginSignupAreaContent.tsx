955ec390e2594ac13f136ddf1565f40aee1efc3d
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-02-08 12:02:27 +0900

    fix: separate LoginSignupAreaContent scss file

--- src/components/Login/LoginSignupAreaContent.tsx
+++ src/components/Login/LoginSignupAreaContent.tsx
@@ -1,38 +1,44 @@
+import '@/assets/styles/components/Login/LoginSignupAreaContent.scss';
 import IconGoogle from '@/assets/images/icon_google.svg';
 import getLoginSignupAreaViewPattern, { LOGIN_SIGNUP_AREA_VIEN_PATTERN } from './getLoginSignupAreaViewPattern';
 
 const LoginSignupContent: React.FC = () => (
-  <>
-    <div className="login-signup-area__input-wrapper">
-      <input className="login-signup-area__input" type="text" name="email" placeholder="email" />
-      <input className="login-signup-area__input" type="password" name="password" placeholder="password" />
-      <div className="login-signup-area__reset-password-link-wrapper">
-        <a className="login-signup-area__reset-password-link" href="/login?reset_password">
+  <div className="login-signup-area-content">
+    <div className="login-signup-area-content__input-wrapper">
+      <input className="login-signup-area-content__input" type="text" name="email" placeholder="email" />
+      <input className="login-signup-area-content__input" type="password" name="password" placeholder="password" />
+      <div className="login-signup-area-content__reset-password-link-wrapper">
+        <a className="login-signup-area-content__reset-password-link" href="/login?reset_password">
           パスワードをお忘れですか？
         </a>
       </div>
     </div>
-    <button className="login-signup-area__submit-button" type="submit">
+    <button className="login-signup-area-content__submit-button" type="submit">
       続行する
     </button>
-    <div className="login-signup-area__border">または</div>
-    <button className="login-signup-area__sns" type="button">
-      <img className="login-signup-area__sns-icon" src={IconGoogle} alt="google" />
-      <span className="login-signup-area__sns-text">Google で続行</span>
+    <div className="login-signup-area-content__border">または</div>
+    <button className="login-signup-area-content__sns" type="button">
+      <img className="login-signup-area-content__sns-icon" src={IconGoogle} alt="google" />
+      <span className="login-signup-area-content__sns-text">Google で続行</span>
     </button>
-  </>
+  </div>
 );
 
 const PasswordResetContent: React.FC = () => (
-  <>
-    <div className="login-signup-area__input-wrapper">
-      <input className="login-signup-area__input" type="text" name="email" placeholder="email" />
-      <input className="login-signup-area__input" type="password" name="new-password" placeholder="new password" />
+  <div className="login-signup-area-content">
+    <div className="login-signup-area-content__input-wrapper">
+      <input className="login-signup-area-content__input" type="text" name="email" placeholder="email" />
+      <input
+        className="login-signup-area-content__input"
+        type="password"
+        name="new-password"
+        placeholder="new password"
+      />
     </div>
-    <button className="login-signup-area__submit-button" type="submit">
+    <button className="login-signup-area-content__submit-button" type="submit">
       再設定する
     </button>
-  </>
+  </div>
 );
 
 const LoginSignupAreaContent: React.FC = () => {
