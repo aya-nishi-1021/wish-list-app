2d1222589488f9984b706c3e3bb75cb10327307d
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-02-09 09:58:04 +0900

    fix: LoginSignupAreaContent component test

--- src/components/Login/LoginSignupAreaContent.spec.tsx
+++ src/components/Login/LoginSignupAreaContent.spec.tsx
@@ -12,11 +12,10 @@ describe('LoginSignupAreaContent', () => {
     expect(wrapper.debug()).toEqual('<PasswordResetContent />');
   });
 
-  // TODO: 登録を完了する用のコンポーネントを作ったら修正する
   it('getLoginSignupAreaViewPattern() の返り値が LOGIN_SIGNUP_AREA_VIEN_PATTERN.SIGNUP_CONFIRM の場合、空の文字列を返す', () => {
     mockLoginSignupAreaViewPattern.mockImplementationOnce(() => LOGIN_SIGNUP_AREA_VIEN_PATTERN.SIGNUP_CONFIRM);
     const wrapper = shallow(<LoginSignupAreaContent />);
-    expect(wrapper.debug()).toEqual('');
+    expect(wrapper.debug()).toEqual('<SignupConfirmContent />');
   });
 
   it('getLoginSignupAreaViewPattern() の返り値が LOGIN_SIGNUP_AREA_VIEN_PATTERN.LOGIN_SIGNUP の場合、LoginSignupContent コンポーネントを返す', () => {
