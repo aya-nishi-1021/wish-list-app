7866d823a8f2c1f96329b3d1c8f9f7799bdc6836
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-02-10 14:26:52 +0900

    feat: handleLoginWithEmail and handleLoginWithGoogle event

--- src/components/Login/LoginSignupAreaContent.tsx
+++ src/components/Login/LoginSignupAreaContent.tsx
@@ -1,30 +1,71 @@
+import React, { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '@/assets/styles/components/Login/LoginSignupAreaContent.scss';
+import { signupWithEmail, loginWithGoogle } from '@/firebase';
 import IconGoogle from '@/assets/images/icon_google.svg';
 import getLoginSignupAreaViewPattern, {
   LOGIN_SIGNUP_AREA_VIEN_PATTERN,
 } from '@/components/Login/getLoginSignupAreaViewPattern';
 
-const LoginSignupContent: React.FC = () => (
-  <div className="login-signup-area-content">
-    <div className="login-signup-area-content__input-wrapper">
-      <input className="login-signup-area-content__input" type="text" name="email" placeholder="email" />
-      <input className="login-signup-area-content__input" type="password" name="password" placeholder="password" />
-      <div className="login-signup-area-content__reset-password-link-wrapper">
-        <a className="login-signup-area-content__reset-password-link" href="/login?reset_password">
-          パスワードをお忘れですか？
-        </a>
+const LoginSignupContent: React.FC = () => {
+  const navigate = useNavigate();
+  const emailInput = React.createRef<HTMLInputElement>();
+  const passwordInput = React.createRef<HTMLInputElement>();
+
+  const handleLoginWithEmail = useCallback(
+    async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      event.preventDefault();
+      const email = emailInput.current?.value;
+      const password = passwordInput.current?.value;
+      if (email && password) await signupWithEmail(email, password);
+      navigate('/');
+    },
+    [emailInput, passwordInput, navigate]
+  );
+
+  const handleLoginWithGoogle = useCallback(
+    async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      event.preventDefault();
+      await loginWithGoogle();
+      navigate('/');
+    },
+    [navigate]
+  );
+
+  return (
+    <div className="login-signup-area-content">
+      <div className="login-signup-area-content__input-wrapper">
+        <input
+          className="login-signup-area-content__input"
+          type="text"
+          name="email"
+          placeholder="email"
+          ref={emailInput}
+        />
+        <input
+          className="login-signup-area-content__input"
+          type="password"
+          name="password"
+          placeholder="password"
+          ref={passwordInput}
+        />
+        <div className="login-signup-area-content__reset-password-link-wrapper">
+          <a className="login-signup-area-content__reset-password-link" href="/login?reset_password">
+            パスワードをお忘れですか？
+          </a>
+        </div>
       </div>
+      <button className="login-signup-area-content__submit-button" type="submit" onClick={handleLoginWithEmail}>
+        続行する
+      </button>
+      <div className="login-signup-area-content__border">または</div>
+      <button className="login-signup-area-content__sns" type="button" onClick={handleLoginWithGoogle}>
+        <img className="login-signup-area-content__sns-icon" src={IconGoogle} alt="google" />
+        <span className="login-signup-area-content__sns-text">Google で続行</span>
+      </button>
     </div>
-    <button className="login-signup-area-content__submit-button" type="submit">
-      続行する
-    </button>
-    <div className="login-signup-area-content__border">または</div>
-    <button className="login-signup-area-content__sns" type="button">
-      <img className="login-signup-area-content__sns-icon" src={IconGoogle} alt="google" />
-      <span className="login-signup-area-content__sns-text">Google で続行</span>
-    </button>
-  </div>
-);
+  );
+};
 
 const PasswordResetContent: React.FC = () => (
   <div className="login-signup-area-content">
