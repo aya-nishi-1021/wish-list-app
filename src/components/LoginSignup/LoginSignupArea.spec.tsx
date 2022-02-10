120113eacb1308e3210748a2d3407b8ea2b80d5a
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-02-10 16:14:13 +0900

    feat: LoginSignupAreaNavi component

--- src/components/LoginSignup/LoginSignupArea.spec.tsx
+++ src/components/LoginSignup/LoginSignupArea.spec.tsx
@@ -1,31 +1,12 @@
 import { shallow } from 'enzyme';
-import getLoginSignupAreaViewPattern, { LOGIN_SIGNUP_AREA_VIEN_PATTERN } from './getLoginSignupAreaViewPattern';
 import LoginSignupArea from './LoginSignupArea';
-import LoginSignupAreaPasswordResetHeading from './LoginSignupAreaPasswordResetHeading';
-import LoginSignupAreaTabs from './LoginSignupAreaTabs';
-import LoginSignupAreaContent from './LoginSignupAreaContent';
-
-jest.mock('./getLoginSignupAreaViewPattern');
-const mockLoginSignupAreaViewPattern = getLoginSignupAreaViewPattern as jest.Mock;
+import LoginSignupAreaNavi from '@/components/LoginSignup/LoginSignupAreaNavi';
+import LoginSignupAreaContent from '@/components/LoginSignup/LoginSignupAreaContent';
 
 describe('LoginSignupArea', () => {
-  it('getLoginSignupAreaViewPattern() の返り値が LOGIN_SIGNUP_AREA_VIEN_PATTERN.PASSWORD_RESET の場合、LoginSignupAreaPasswordResetHeading コンポーネントが表示される', () => {
-    mockLoginSignupAreaViewPattern.mockImplementationOnce(() => LOGIN_SIGNUP_AREA_VIEN_PATTERN.PASSWORD_RESET);
+  it('LoginSignupAreaNavi コンポーネントと LoginSignupAreaContent コンポーネントが表示される', () => {
     const wrapper = shallow(<LoginSignupArea />);
-    expect(wrapper.find('.login-signup-area').contains(<LoginSignupAreaPasswordResetHeading />)).toBeTruthy();
-  });
-  it('getLoginSignupAreaViewPattern() の返り値が LOGIN_SIGNUP_AREA_VIEN_PATTERN.LOGIN の場合、LoginSignupAreaTabs コンポーネントが表示される', () => {
-    mockLoginSignupAreaViewPattern.mockImplementationOnce(() => LOGIN_SIGNUP_AREA_VIEN_PATTERN.LOGIN);
-    const wrapper = shallow(<LoginSignupArea />);
-    expect(wrapper.find('.login-signup-area').contains(<LoginSignupAreaTabs />)).toBeTruthy();
-  });
-  it('getLoginSignupAreaViewPattern() の返り値が LOGIN_SIGNUP_AREA_VIEN_PATTERN.SIGNUP の場合、LoginSignupAreaTabs コンポーネントが表示される', () => {
-    mockLoginSignupAreaViewPattern.mockImplementationOnce(() => LOGIN_SIGNUP_AREA_VIEN_PATTERN.SIGNUP);
-    const wrapper = shallow(<LoginSignupArea />);
-    expect(wrapper.find('.login-signup-area').contains(<LoginSignupAreaTabs />)).toBeTruthy();
-  });
-  it('LoginSignupAreaContent コンポーネントが表示される', () => {
-    const wrapper = shallow(<LoginSignupArea />);
+    expect(wrapper.find('.login-signup-area').contains(<LoginSignupAreaNavi />)).toBeTruthy();
     expect(wrapper.find('.login-signup-area').contains(<LoginSignupAreaContent />)).toBeTruthy();
   });
 });
--- src/components/LoginSignup/LoginSignupArea.tsx
+++ src/components/LoginSignup/LoginSignupArea.tsx
@@ -1,24 +1,12 @@
 import '@/assets/styles/components/LoginSignup/LoginSignupArea.scss';
-import getLoginSignupAreaViewPattern, { LOGIN_SIGNUP_AREA_VIEN_PATTERN } from './getLoginSignupAreaViewPattern';
-import LoginSignupAreaPasswordResetHeading from './LoginSignupAreaPasswordResetHeading';
-import LoginSignupAreaTabs from '@/components/LoginSignup/LoginSignupAreaTabs';
+import LoginSignupAreaNavi from '@/components/LoginSignup/LoginSignupAreaNavi';
 import LoginSignupAreaContent from '@/components/LoginSignup/LoginSignupAreaContent';
 
-const LoginSignupArea: React.FC = () => {
-  const loginSignupAreaViewPattern = getLoginSignupAreaViewPattern();
-  const heading =
-    loginSignupAreaViewPattern === LOGIN_SIGNUP_AREA_VIEN_PATTERN.PASSWORD_RESET ? (
-      <LoginSignupAreaPasswordResetHeading />
-    ) : (
-      <LoginSignupAreaTabs />
-    );
-
-  return (
-    <div className="login-signup-area">
-      {heading}
-      <LoginSignupAreaContent />
-    </div>
-  );
-};
+const LoginSignupArea: React.FC = () => (
+  <div className="login-signup-area">
+    <LoginSignupAreaNavi />
+    <LoginSignupAreaContent />
+  </div>
+);
 
 export default LoginSignupArea;
