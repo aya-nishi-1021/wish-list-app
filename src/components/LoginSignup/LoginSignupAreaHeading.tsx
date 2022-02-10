e05b793c0931cf3f99725cd7f4d72ec066cf793a
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-02-10 15:16:01 +0900

    fix: LoginSignupAreaHeading to Tabs

--- src/components/LoginSignup/LoginSignupAreaHeading.tsx
+++ src/components/LoginSignup/LoginSignupAreaHeading.tsx
@@ -20,22 +20,39 @@ const getHeadingText = (): string => {
 const LoginSignupAreaHeading: React.FC = () => {
   const headingText = getHeadingText();
   const isBackButtonShow = !!window.location.search;
+  const { pathname } = window.location;
 
   return (
-    <div className="login-signup-area-heading">
+    <>
       {isBackButtonShow && (
-        <button className="login-signup-area-heading__back-button" type="button" onClick={() => window.history.back()}>
-          <img src={IconArrow} alt="back login page" />
-        </button>
+        <div className="login-signup-area-heading">
+          <button
+            className="login-signup-area-heading__back-button"
+            type="button"
+            onClick={() => window.history.back()}
+          >
+            <img src={IconArrow} alt="back login page" />
+          </button>
+          <h3
+            className={`login-signup-area-heading__text ${
+              isBackButtonShow ? 'login-signup-area-heading__text--back-button-show' : ''
+            }`}
+          >
+            {headingText}
+          </h3>
+        </div>
       )}
-      <h3
-        className={`login-signup-area-heading__text ${
-          isBackButtonShow ? 'login-signup-area-heading__text--back-button-show' : ''
-        }`}
-      >
-        {headingText}
-      </h3>
-    </div>
+      {!isBackButtonShow && (
+        <ul className="login-signup-area-tabs">
+          <li className={`login-signup-area-tab ${pathname === '/login' ? 'login-signup-area-tab--focused' : ''}`}>
+            <a href="/login">ログイン</a>
+          </li>
+          <li className={`login-signup-area-tab ${pathname === '/signup' ? 'login-signup-area-tab--focused' : ''}`}>
+            <a href="/signup">新規登録</a>
+          </li>
+        </ul>
+      )}
+    </>
   );
 };
 
