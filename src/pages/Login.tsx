eb3367f3e2619b46a308b4af837834ab25ac168d
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-02-10 14:26:13 +0900

    feat: login page useEffect

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,25 @@
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '@/assets/styles/pages/Login.scss';
+import { FirebaseContext } from '@/contexts';
 import LoginSignupArea from '@/components/Login/LoginSignupArea';
 
-const Login: React.FC = () => (
-  <div className="login-signup">
-    <h2 className="login-signup__title">Food Wish List</h2>
-    <LoginSignupArea />
-  </div>
-);
+const Login: React.FC = () => {
+  // ログイン済みかどうかのフラグ
+  const isAuthenticated = useContext(FirebaseContext).user;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // ログイン済の場合はホーム画面に遷移させる
+    if (isAuthenticated) navigate('/');
+  }, [isAuthenticated, navigate]);
+
+  return (
+    <div className="login-signup">
+      <h2 className="login-signup__title">Food Wish List</h2>
+      <LoginSignupArea />
+    </div>
+  );
+};
 
 export default Login;
