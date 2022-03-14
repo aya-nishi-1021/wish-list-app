1e482b1a7b1fc1d9e6a0b5f71ee85aeda3000c0c
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-03-14 14:14:01 +0900

    feat: Overlay component

---
+++ src/components/Common/Overlay.tsx
@@ -0,0 +1,24 @@
+import '@/assets/styles/components/Common/Overlay.scss';
+import React from 'react';
+
+type Props = {
+  isShow: boolean;
+  hideOverlay: VoidFunction;
+};
+
+const Overlay: React.FC<Props> = ({ children, isShow = false, hideOverlay }) => {
+  const handleClickOverlay = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    // ダイアログの外側をクリックしたときのみオーバーレイを非表示にする
+    if (event.target === event.currentTarget) hideOverlay();
+  };
+
+  if (!isShow) return null;
+
+  return (
+    <div className="overlay" onClick={handleClickOverlay} aria-hidden="true">
+      {children}
+    </div>
+  );
+};
+
+export default Overlay;
