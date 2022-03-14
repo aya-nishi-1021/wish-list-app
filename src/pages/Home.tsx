c1e7997cf56f0831a78cca60f7a5abf4422c6fa3
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-03-14 14:15:09 +0900

    feat: show AddShopDialog in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,28 @@
 import '@/assets/styles/pages/Home.scss';
+import { useState } from 'react';
+import Overlay from '@/components/Common/Overlay';
+import AddShopDialog from '@/components/Home/AddShopDialog';
 import Header from '@/components/Common/Header';
 import ShopList from '@/components/Home/ShopList';
 import MapView from '@/components/Home/MapView';
 import BottomNavi from '@/components/Common/BottomNavi';
 
-const Home: React.FC = () => (
-  <div className="home">
-    <Header isSearchBoxShow isAddShopButtonShow />
-    <div className="home__content-wrapper">
-      <ShopList />
-      <MapView />
+const Home: React.FC = () => {
+  const [isAddShopDialogShow, setIsAddShopDialogShow] = useState(false);
+
+  return (
+    <div className="home">
+      <Overlay isShow={isAddShopDialogShow} hideOverlay={() => setIsAddShopDialogShow(false)}>
+        <AddShopDialog />
+      </Overlay>
+      <Header isSearchBoxShow isAddShopButtonShow handleClickAddShopButton={() => setIsAddShopDialogShow(true)} />
+      <div className="home__content-wrapper">
+        <ShopList />
+        <MapView />
+      </div>
+      <BottomNavi isAddShopButtonShow />
     </div>
-    <BottomNavi isAddShopButtonShow />
-  </div>
-);
+  );
+};
 
 export default Home;
