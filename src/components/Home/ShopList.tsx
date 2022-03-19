f1d4a06970a1f07d4ef7da641e726f09c9acdb64
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-03-19 15:52:09 +0900

    feat: PC view markup in Home

--- src/components/Home/ShopList.tsx
+++ src/components/Home/ShopList.tsx
@@ -1,3 +1,5 @@
+import '@/assets/styles/components/Home/ShopList.scss';
+
 const ShopList: React.FC = () => <div className="shop-list">ショップリスト</div>;
 
 export default ShopList;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,7 @@ import BottomNavi from '@/components/Common/BottomNavi';
 
 const Home: React.FC = () => {
   const [isAddShopDialogShow, setIsAddShopDialogShow] = useState(false);
+  const [isMapViewExpanded, setIsMapViewExpanded] = useState(false);
 
   return (
     <div className="home">
@@ -17,8 +18,16 @@ const Home: React.FC = () => {
       </Overlay>
       <Header isSearchBoxShow isAddShopButtonShow handleClickAddShopButton={() => setIsAddShopDialogShow(true)} />
       <div className="home__content-wrapper">
-        <ShopList />
-        <MapView />
+        <div
+          className={`home__shop-list-wrapper${isMapViewExpanded ? ' home__shop-list-wrapper--map-view-expanded' : ''}`}
+        >
+          <ShopList />
+        </div>
+        <div
+          className={`home__map-view-wrapper${isMapViewExpanded ? ' home__map-view-wrapper--map-view-expanded' : ''}`}
+        >
+          <MapView expandView={() => setIsMapViewExpanded(true)} contractView={() => setIsMapViewExpanded(false)} />
+        </div>
       </div>
       <BottomNavi isAddShopButtonShow handleClickAddShopButton={() => setIsAddShopDialogShow(true)} />
     </div>
