9578ad22e22fca0318d1fc24151e86a7f2cccb3e
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-03-19 15:51:33 +0900

    feat: view-area-scale-button in MapView component

--- src/components/Home/MapView.tsx
+++ src/components/Home/MapView.tsx
@@ -1,3 +1,39 @@
-const MapView: React.FC = () => <div className="map-view">マップ</div>;
+import { useState } from 'react';
+import '@/assets/styles/components/Home/MapView.scss';
+import IconArrow from '@/assets/images/icon_arrow.svg';
+
+type Props = {
+  expandView: VoidFunction;
+  contractView: VoidFunction;
+};
+
+const MapView: React.FC<Props> = ({ expandView, contractView }) => {
+  const [isMapViewAreaExpanded, setIsMapViewAreaExpanded] = useState(false);
+  const viewAreaScaleButtonImgAlt = isMapViewAreaExpanded ? 'リストを表示する' : '地図エリアを拡大する';
+  const handleClickViewAreaScaleButton = () => {
+    if (isMapViewAreaExpanded) {
+      contractView();
+      setIsMapViewAreaExpanded(false);
+    } else {
+      expandView();
+      setIsMapViewAreaExpanded(true);
+    }
+  };
+
+  return (
+    <div className="map-view">
+      <button type="button" className="map-view__view-area-scale-button" onClick={handleClickViewAreaScaleButton}>
+        <img
+          src={IconArrow}
+          alt={viewAreaScaleButtonImgAlt}
+          className={`map-view__view-area-scale-button__img${
+            isMapViewAreaExpanded ? ' map-view__view-area-scale-button__img--map-view-area-expanded' : ''
+          }`}
+        />
+        {isMapViewAreaExpanded && <div className="map-view__view-area-scale-button__text">リストを表示</div>}
+      </button>
+    </div>
+  );
+};
 
 export default MapView;
