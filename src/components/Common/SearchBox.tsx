7cf781285d08fd2993a3f3d89e3b9b36a1e5f4a2
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-03-29 13:14:41 +0900

    feat: SearchBox component props value and changeValue

--- src/components/Common/SearchBox.tsx
+++ src/components/Common/SearchBox.tsx
@@ -2,12 +2,14 @@ import '@/assets/styles/components/Common/SearchBox.scss';
 import IconSearch from '@/assets/images/icon_search.svg';
 
 type Props = {
+  value: string;
+  changeValue: (event: React.ChangeEvent<HTMLInputElement>) => void;
   handleClickSearchButton: VoidFunction;
 };
 
-const SearchBox: React.FC<Props> = ({ handleClickSearchButton }) => (
+const SearchBox: React.FC<Props> = ({ value, changeValue, handleClickSearchButton }) => (
   <div className="search-box">
-    <input type="text" className="search-box__input" />
+    <input type="text" className="search-box__input" value={value} onChange={(e) => changeValue(e)} />
     <button type="button" className="search-box__button" onClick={handleClickSearchButton}>
       <img src={IconSearch} alt="検索" />
     </button>
