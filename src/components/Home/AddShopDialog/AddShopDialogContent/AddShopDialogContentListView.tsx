436a26f815a36ec52990051031a6f73f24234abc
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-04-02 17:33:44 +0900

    feat: images in type ShopList

--- src/components/Home/AddShopDialog/AddShopDialogContent/AddShopDialogContentListView.tsx
+++ src/components/Home/AddShopDialog/AddShopDialogContent/AddShopDialogContentListView.tsx
@@ -41,6 +41,7 @@ const AddShopDialogContentListView: React.FC<Props> = ({
         if (!r) return;
         void addWishList({
           placeId: r.place_id,
+          images: r.photos ? r.photos.map((photo: google.maps.places.PlacePhoto) => photo.getUrl()) : [],
           name: r.name,
           rating: r.rating,
           phoneNumber: r.formatted_phone_number,
