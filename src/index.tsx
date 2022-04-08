fc7aeb4258e915010a4b0285830c973868b96b2d
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-04-08 16:28:58 +0900

    move: LoadScript from Home to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,29 +3,37 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'ress';
 import '@/assets/styles/foundation/base.scss';
+import { LoadScript } from '@react-google-maps/api';
+import { FirebaseProvider } from './firebase';
 import Login from '@/pages/Login';
 import Signup from '@/pages/Signup';
 import PrivateRoute from '@/components/PrivateRoute';
 import Home from '@/pages/Home';
 import MyPage from '@/pages/MyPage';
+import ShopDetail from '@/pages/shop/_name';
 import reportWebVitals from '@/reportWebVitals';
-import { FirebaseProvider } from './firebase';
-import ShopDetail from './pages/shop/_name';
+
+const key = process.env.REACT_APP_GOOGLE_MAP_API_KEY as string;
+
+type Libraries = ('drawing' | 'geometry' | 'localContext' | 'places' | 'visualization')[];
+const libraries: Libraries = ['places'];
 
 ReactDOM.render(
   <React.StrictMode>
     <FirebaseProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<PrivateRoute />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/shop/:name" element={<ShopDetail />} />
-            <Route path="/mypage" element={<MyPage />} />
-          </Route>
-        </Routes>
-      </Router>
+      <LoadScript googleMapsApiKey={key} libraries={libraries}>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<PrivateRoute />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/shop/:name" element={<ShopDetail />} />
+              <Route path="/mypage" element={<MyPage />} />
+            </Route>
+          </Routes>
+        </Router>
+      </LoadScript>
     </FirebaseProvider>
   </React.StrictMode>,
   document.getElementById('root')
