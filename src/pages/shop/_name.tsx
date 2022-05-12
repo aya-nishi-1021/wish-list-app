4fc84a82501d51d10cfd0df20fdfc621471750e1
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-05-12 09:42:46 +0900

    fix: shop detail page shop image fetch

--- src/pages/shop/_name.tsx
+++ src/pages/shop/_name.tsx
@@ -12,10 +12,22 @@ const ShopDetail: React.FC = () => {
   const { isOpen } = location.state as { isOpen: boolean };
 
   const [isOpeningHoursShow, setIsOpeningHoursShow] = useState(false);
+  const [shopImages, setShopImages] = useState<string[]>([]);
 
   const handleToggleOpeningHours = () => {
     setIsOpeningHoursShow(!isOpeningHoursShow);
   };
+
+  const { google } = window;
+  const service = new google.maps.places.PlacesService(document.createElement('div'));
+
+  if (!shopInfo.placeId) return null;
+  service.getDetails({ placeId: shopInfo.placeId }, (r, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      if (!r) return;
+      setShopImages(r.photos ? r.photos.map((photo) => photo.getUrl()) : []);
+    }
+  });
 
   return (
     <div className="shop-detail">
@@ -56,7 +68,7 @@ const ShopDetail: React.FC = () => {
         )}
         <div className="shop-detail__item">住所: {shopInfo.address || '-'}</div>
         <div className="shop-detail__image-wrapper">
-          {shopInfo.images.map((image) => (
+          {shopImages.map((image) => (
             <div className="shop-detail__image" key={image}>
               <img src={image} alt="お店の写真" />
             </div>
