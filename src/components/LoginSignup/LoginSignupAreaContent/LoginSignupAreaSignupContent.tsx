f0ec48166c548430ad185e61d03dc59ad5180413
Ayaka Nagasaka <nishiu.ayaka@gmail.com>
2022-10-16 15:09:43 +0900

    fix: replace LoginSignupAreaSignupContent with react-hook-form

--- src/components/LoginSignup/LoginSignupAreaContent/LoginSignupAreaSignupContent.tsx
+++ src/components/LoginSignup/LoginSignupAreaContent/LoginSignupAreaSignupContent.tsx
@@ -1,54 +1,56 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { signupWithEmail } from '@/firebase';
 
 const LoginSignupAreaSignupContent: React.FC = () => {
   const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const [emailInputValue, setEmailInputValue] = useState('');
-  const [passwordInputValue, setPasswordInputValue] = useState('');
 
-  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (errorMessage) setErrorMessage(null);
-    setEmailInputValue(event.target.value);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  type FormValues = {
+    email: string;
+    password: string;
   };
-
-  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (errorMessage) setErrorMessage(null);
-    setPasswordInputValue(event.target.value);
-  };
-
-  const handleSignupWithEmail = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    await signupWithEmail(emailInputValue, passwordInputValue)
+  const handleSignupWithEmail = async (data: FormValues) => {
+    await signupWithEmail(data.email, data.password)
       .then(() => navigate('/'))
       .catch((error: Error) => setErrorMessage(error.message));
   };
 
   return (
-    <form className="login-signup-area-content" onSubmit={handleSignupWithEmail}>
-      {errorMessage && <div className="login-signup-area-content__error-message">{errorMessage}</div>}
+    <form className="login-signup-area-content" onSubmit={handleSubmit(handleSignupWithEmail)}>
+      {errorMessage && (
+        <div className="login-signup-area-content__error-message login-signup-area-content__error-message--firebase">
+          {errorMessage}
+        </div>
+      )}
       <div className="login-signup-area-content__input-wrapper">
+        {errors.email?.message && (
+          <div className="login-signup-area-content__error-message">{errors.email?.message}</div>
+        )}
         <input
           className="login-signup-area-content__input"
           type="text"
-          name="email"
           placeholder="email"
-          onChange={handleChangeEmail}
+          {...register('email', { required: { value: true, message: '入力が必須の項目です。' } })}
         />
+        {errors.password?.message && (
+          <div className="login-signup-area-content__error-message">{errors.password.message}</div>
+        )}
         <input
           className="login-signup-area-content__input"
           type="password"
-          name="password"
           placeholder="password"
-          onChange={handleChangePassword}
+          {...register('password', { required: { value: true, message: '入力が必須の項目です。' } })}
         />
       </div>
-      <button
-        className="login-signup-area-content__submit-button"
-        type="submit"
-        disabled={!emailInputValue || !passwordInputValue}
-      >
+      <button className="login-signup-area-content__submit-button" type="submit">
         続行する
       </button>
     </form>
